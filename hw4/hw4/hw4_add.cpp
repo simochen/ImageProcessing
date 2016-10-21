36411607806a29f603d106ddfa3fc69a28b31205
simochen <chenx324@yeah.net>
2016-10-21 13:12:00 +0800

    hw4

--- hw4/hw4/hw4_add.cpp
+++ hw4/hw4/hw4_add.cpp
@@ -35,27 +35,41 @@ Mat performDFT(const Mat &srcImage)
 	return dstImage;
 }
 
-Mat calcNoise(const Mat &srcDFT, const Mat &blurDFT) {
-	Mat srcPlane[2], blurPlane[2], noisePlane[2];
-	split(srcDFT, srcPlane);
-	split(blurDFT, blurPlane);
-	Mat mul[6];
-	multiply(blurPlane[0], blurPlane[0], mul[0]);
-	multiply(blurPlane[1], blurPlane[1], mul[1]);
-	multiply(srcPlane[0], blurPlane[0], mul[2]);
-	multiply(srcPlane[1], blurPlane[1], mul[3]);
-	multiply(srcPlane[1], blurPlane[0], mul[4]);
-	multiply(srcPlane[0], blurPlane[1], mul[5]);
-	divide(mul[2] + mul[3], mul[0] + mul[1], noisePlane[0]);
-	divide(mul[4] - mul[5], mul[0] + mul[1], noisePlane[1]);
+Mat performIDFT(const Mat &srcImage)
+{
+	Mat dstImage;
+	//idft，取实部
+	idft(srcImage, dstImage, DFT_REAL_OUTPUT);
+	//中心化
+	centralize(dstImage);
+	//归一化
+	normalize(dstImage, dstImage, 0, 1, CV_MINMAX);
+	return dstImage;
+}
 
+Mat complexDiv(const Mat &cMat1, const Mat &cMat2, double scale = 1) {
+	Mat plane1[2], plane2[2], divPlane[2];
+	split(cMat1, plane1);
+	split(cMat2, plane2);
+	Mat mul[6];
+	multiply(plane2[0], plane2[0], mul[0]);
+	multiply(plane2[1], plane2[1], mul[1]);
+	multiply(plane1[0], plane2[0], mul[2]);
+	multiply(plane1[1], plane2[1], mul[3]);
+	multiply(plane1[1], plane2[0], mul[4]);
+	multiply(plane1[0], plane2[1], mul[5]);
+	divide(mul[2] + mul[3], mul[0] + mul[1], divPlane[0], scale);
+	divide(mul[4] - mul[5], mul[0] + mul[1], divPlane[1], scale);
+	Mat divMat;
+	merge(divPlane, 2, divMat);
+	return divMat;
 }
 
 int main() {
 	//读取原图
 	Mat srcImage, graySrc;
 	srcImage = imread("lena.jpg");
-	srcImage = srcImage(Rect(32, 12, 320, 320));
+	srcImage = srcImage(Rect(30, 12, 320, 320));
 	cvtColor(srcImage, graySrc, CV_RGB2GRAY);
 	imshow("Source Image", srcImage);
 	imshow("Gray Source Image", graySrc);
@@ -68,9 +82,18 @@ int main() {
 	cvtColor(blurImage, grayBlur, CV_RGB2GRAY);
 	imshow("Resized Blur Image", blurImage);
 	imshow("Gray Blur Image", grayBlur);
-	Mat srcDFT, blurDFT;
-	srcDFT = performDFT(graySrc);
-	blurDFT = performDFT(grayBlur);
+	Mat srcF, blurF, degF;
+	srcF = performDFT(graySrc);
+	blurF = performDFT(grayBlur);
+	degF = complexDiv(blurF, srcF);
+	Mat blurPlane[3], est_srcN[3];
+	split(blurImage, blurPlane);
+	est_srcN[0] = performIDFT(complexDiv(performDFT(blurPlane[0]), degF));
+	est_srcN[1] = performIDFT(complexDiv(performDFT(blurPlane[1]), degF));
+	est_srcN[2] = performIDFT(complexDiv(performDFT(blurPlane[2]), degF));
+	Mat est_srcImage;
+	merge(est_srcN, 3, est_srcImage);
+	imshow("Estimate Source Image", est_srcImage);
 	waitKey(0);
 
 	return 0;
